bec38410c4c8373dc7bc83bebf33ec8e621e3545
AlyAzeemi <alykhawar@gmail.com>
2021-07-27 12:50:00 +0500

    Added switch comp and dashboard

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./components/login/Login";
 import Signup from "./components/login/Signup";
 import LandingPage from "./components/login/LandingPage";
 import Header from "./components/login/Header";
 import ResetPassword from "./components/login/ResetPassword";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <Router>
       <div className="container">
         <Header />
-        <Route path="/" component={LandingPage} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/resetpassword" component={ResetPassword} />
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/resetpassword" component={ResetPassword} />
+          <Route path="/dashboard" component={Dashboard} />
+        </Switch>
       </div>
     </Router>
   );
--- src/components/login/LandingPage.js
+++ src/components/login/LandingPage.js
@@ -1,22 +1,17 @@
 import { Link } from "react-router-dom";
-import { useLocation } from "react-router";
+
 const LandingPage = () => {
-  const location = useLocation();
-  if (location.pathname === "/") {
-    return (
-      <div>
-        <header className="header">Welcome. Pick one to proceed.</header>
-        <Link to="/login">
-          <button className="btn btn-block">Login</button>
-        </Link>
-        <Link to="/Signup">
-          <button className="btn btn-block">Signup</button>
-        </Link>
-      </div>
-    );
-  } else {
-    return <></>;
-  }
+  return (
+    <div>
+      <header className="header">Welcome. Pick one to proceed.</header>
+      <Link to="/login">
+        <button className="btn btn-block">Login</button>
+      </Link>
+      <Link to="/signup">
+        <button className="btn btn-block">Signup</button>
+      </Link>
+    </div>
+  );
 };
 
 export default LandingPage;
