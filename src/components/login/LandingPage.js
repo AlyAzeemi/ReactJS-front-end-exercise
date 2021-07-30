1aeaabc31baebe48f498b91bbf8b06fdbccb9db7
AlyAzeemi <alykhawar@gmail.com>
2021-07-30 16:51:24 +0500

    remaking landing page

--- src/components/login/LandingPage.js
+++ src/components/login/LandingPage.js
@@ -1,18 +1,54 @@
 import { Link } from "react-router-dom";
-import Header from "./Header";
+import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Paper } from "@material-ui/core";
+import { Container } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    alignItems: "center",
+    direction: "row",
+    justifyContent: "center",
+  },
+  paperSignUp: {
+    backgroundColor: "#D5BEFF",
+    padding: theme.spacing(2),
+    textAlign: "center",
+    width: "500",
+    height: "300",
+  },
+  paperLogIn: {
+    backgroundColor: "#BEFFD5",
+    padding: theme.spacing(2),
+    textAlign: "center",
+    width: "500",
+    height: "300",
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
 
 const LandingPage = () => {
+  const classes = useStyles();
   return (
-    <div className="container">
-      <Header />
-      <header className="header">Welcome. Pick one to proceed.</header>
-      <Link to="/login">
-        <button className="btn btn-block">Login</button>
-      </Link>
-      <Link to="/signup">
-        <button className="btn btn-block">Signup</button>
-      </Link>
-    </div>
+    <Container>
+      <Grid container className={classes.root} spacing={2}>
+        {["LogIn", "SignUp"].map((text) => (
+          <Link to={`/${text}`} style={{ textDecoration: "none" }}>
+            <Grid item xs={12} md={6} lg={3}>
+              <Paper className={classes[`paper${text}`]} elevation={3}>
+                <Typography variant="h2" style={{ color: "white" }}>
+                  {text}
+                </Typography>
+              </Paper>
+            </Grid>
+          </Link>
+        ))}
+      </Grid>
+    </Container>
   );
 };
 
